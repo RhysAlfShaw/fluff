ba0e40b226d0fd896d41d742cf2975630052b024
Rhys Shaw <rhysc3p0@gmail.com>
2024-05-23 22:24:02 +0100

    rust tut

---
+++ math/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; 
+
+    // remainder
+    let remainder = 43 % 5;
+
+    let t = true;
+    
+    let f: bool = false; 
+
+    println!("{} {} {} {} {} {} {}", sum, difference, product, quotient,truncated,remainder, f);
+
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("{}{}{}", c, z, heart_eyed_cat)
+}
