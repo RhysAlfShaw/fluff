ac3dea6143c1d1b18e430720fb01789710677b2c
Rhys Shaw <rhysc3p0@gmail.com>
2024-05-23 23:18:24 +0100

    Create main.rs

---
+++ ownership/main.rs
@@ -0,0 +1,28 @@
+fn main () {
+    let s = "hello";
+
+    {                      // s is not valid here, it’s not yet declared
+        let s = "hello";   // s is valid from this point forward
+
+        // do stuff with s
+    }                      // this scope is now over, and s is no longer valid
+    
+    let s = String::from("hello");
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+
+}
